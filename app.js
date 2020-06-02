36a530b644cf64270c7596ac01b1951de86c98ed
meeramenonpgms <admin@Admins-MacBook-Pro.local>
2020-06-02 15:35:10 +0530

    ejs added

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express=require('express')
+var bodypaser=require('body-parser');
 
 const server=express()
+server.set("view engine","ejs");
+server.use(bodypaser.urlencoded({extended:true}));
+
 
 server.listen(8000,function(){
     console.log("server started listening...")
@@ -8,13 +12,25 @@ server.listen(8000,function(){
 
 
 server.get("/",function(req,res){
-    res.sendFile(__dirname+"/views/home.html")
+    res.render("login")
 
 })
 server.get("/home",function(req,res){
-    res.send("this home page")
+    res.render("home")
 })
 server.get("/login",function(req,res){
     res.sendFile(__dirname+"/views/login.html")
     
+})
+server.get("/services",function(req,res){
+    res.render("services")
+})
+server.post("/validate",function(req,res){
+    console.log(req.body.user)
+    if(req.body.user=="admin"){
+        res.send("login success")
+    }
+    else{
+        res.send("login failed")
+    }
 })
